8c5216ae1591787ccb7b80a9a41200ca42a62758
karmanya1804 <karmanya1804@gmail.com>
2024-05-11 23:57:53 -0400

    Sockets

--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -1,23 +1,65 @@
-import { useContext } from "react";
+import React, { useEffect, useState, useRef } from 'react';
 import AceEditor from "react-ace";
-import { LanguageContext } from "../contexts";
 
-function CodeEditor() {
-  const { language } = useContext(LanguageContext);
-  
+function CodeEditor({ roomId }) {
+  const [code, setCode] = useState('');
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize WebSocket connection
+    socketRef.current = new WebSocket(`wss://3laaw8k2df.execute-api.us-east-1.amazonaws.com/production/?roomId=${roomId}`);
+
+    socketRef.current.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    socketRef.current.onmessage = (event) => {
+      const { action, code } = JSON.parse(event.data);
+      if (action === 'updateCode') {
+        setCode(code);
+      }
+    };
+
+    socketRef.current.onclose = () => {
+      console.log('WebSocket disconnected');
+    };
+
+    socketRef.current.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    // Clean up the WebSocket connection when the component unmounts
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.close();
+      }
+    };
+  }, [roomId]);
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    console.log(newCode)
+    // Send the new code state over the WebSocket
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      console.log("sending request")
+      socketRef.current.send(JSON.stringify({ action: 'updateCode',  roomId: roomId, code: newCode }));
+    }
+  };
+
   return (
     <div className="h-full w-full">
       <AceEditor
         mode="python"
         theme="solarized_light"
         name="code-editor"
-        value={language["python"]}
+        onChange={handleCodeChange}
+        value={code}
         fontSize={14}
         width="100%"
         height="100%"
         highlightActiveLine={false}
         showPrintMargin={false}
-        editorProps={{ $blockScrolling: false }}
+        editorProps={{ $blockScrolling: true }}
         setOptions={{
           useWorker: false,
           fontFamily: "SourceCodePro",
--- frontend/src/pages/CodeDashboard.jsx
+++ frontend/src/pages/CodeDashboard.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 
 import { NavBar, CodeEditor, OutputEditor, InputEditor } from "../components";
-import { LanguageContextProvider } from "../contexts";
+import { LanguageContextProvider} from "../contexts";
+import { useParams } from "react-router-dom";
 
 function CodeDashboard() {
+  const { roomId } = useParams();
+
+  console.log("Current roomId:", roomId);
   return (
-    <LanguageContextProvider>
-      <div className="flex flex-col h-screen w-screen gap-3 bg-[#DDD6C1] dark:bg-[#00212B] px-3 py-3">
-        <NavBar />
-        <CodeEditor />
-        <div className="h-full w-full flex gap-3">
-          <InputEditor />
-          <OutputEditor />
+      <LanguageContextProvider>
+        <div className="flex flex-col h-screen w-screen gap-3 bg-[#DDD6C1] dark:bg-[#00212B] px-3 py-3">
+          <NavBar />
+          <CodeEditor roomId={roomId}/>
+          <div className="h-full w-full flex gap-3">
+            <InputEditor />
+            <OutputEditor />
+          </div>
         </div>
-      </div>
-    </LanguageContextProvider>
+      </LanguageContextProvider>
   )
 }
 
